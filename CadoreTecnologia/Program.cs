e0ad342b896f767fc5c298232f83b65dae6e32dc
cadore <cadore.william@gmail.com>
2015-08-17 12:19:34 -0400

    initial commit

---
+++ CadoreTecnologia/Program.cs
@@ -0,0 +1,26 @@
+﻿using CadoreTecnologia.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CadoreTecnologia
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            LoginForm lf = new LoginForm();
+            DialogResult rs = lf.ShowDialog();
+            if(rs == DialogResult.OK)
+                Application.Run(new DesktopForm());
+        }
+    }
+}
